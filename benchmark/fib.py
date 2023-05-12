8797ae2215bd2245fa7fd6e2a0515a9e11a3dc04
Sourcery AI <>
2023-05-12 06:36:19 +0000

    'Refactored by Sourcery'

--- benchmark/fib.py
+++ benchmark/fib.py
@@ -1,10 +1,9 @@
 import sys
 
 def fib(n):
-  if n < 2: return n
-  return fib(n - 1) + fib(n - 2)
+  return n if n < 2 else fib(n - 1) + fib(n - 2)
 
 n = int(sys.argv[1])
 m = int(sys.argv[2])
-for i in range(0, n):
+for _ in range(0, n):
   print(fib(m))
